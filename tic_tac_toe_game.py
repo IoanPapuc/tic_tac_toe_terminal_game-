3f2a7e8d96f77c06bc8c769c79b8c249392e8c5d
Ioan Papuc <papuc_ionut27@yahoo.com>
2024-03-31 21:56:42 +0300

    Added inserting character into board functionality

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -12,8 +12,23 @@ for n in range(2):
     board.append(["_"] * 3)
 board.append([" "] * 3)
 
-# print(board)
 
 def print_board(board):
     for row in board:
         print(" | ".join(row))
+
+
+def insert_symbol(board, symbol):
+    print("Time for {S}:".format(S = symbol))
+    row = int(input("Enter row: "))
+    col = int(input("Enter column: "))
+    while row not in range(1,4) or col not in range(0,4):
+        print("You missed the board! Enter a valid location for {S}:".format(S = symbol))
+        row = int(input("Enter row: "))
+        col = int(input("Enter column: "))
+    while board[row-1][col-1] == "X" or board[row-1][col-1] == "O":
+        print("Choose an empty box for {S}:".format(S = symbol))
+        row = int(input("Enter row: "))
+        col = int(input("Enter column: "))
+    board[row-1][col-1] = symbol.upper()
+    print_board(board)
