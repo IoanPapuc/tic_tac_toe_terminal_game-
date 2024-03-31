cc92eb1a637aecd500e6ddd458faa7e460077e6c
Ioan Papuc <papuc_ionut27@yahoo.com>
2024-03-31 21:17:49 +0300

    first commit

---
+++ tic_tac_toe_game.py
@@ -0,0 +1,8 @@
+"""
+In this file it is created a terminal version of the Tic Tac Toe 
+game (X and O) which can be played by two users. The players are 
+asked successivelly in the terminal to input the location (row and
+column) where they want their specific character to be inserted.
+The game ends when one of them first satisfies one of the three
+winning conditions.
+"""
